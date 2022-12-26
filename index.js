3b1c6b820d144aecd63417908d0206eb403fbf21
ughurmadinali <120271577+ughurmadinali@users.noreply.github.com>
2022-12-26 19:50:07 +0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+const price = 2
+const amount = 25
+console.log(price * amount)
+
+const price = 3
+const amount = 30
+console.log(price * amount)
+
+let year = 2012
+let day birth = 10
+console.log(year * day birth)
